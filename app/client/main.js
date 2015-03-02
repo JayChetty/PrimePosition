da2cd84ad584a9baa129b71afbdc758e0ed3f326
JayChetty <jaypaulchetty@gmail.com>
2015-03-02 22:35:00 +0000

    option to change structure

--- app/client/main.js
+++ app/client/main.js
@@ -7,7 +7,7 @@ window.onload = function(){
 
   // create a renderer instance.
   var renderer = PIXI.autoDetectRenderer(800, 800);
-  var team = new Team(15);
+  var team = new Team(30);
   var texture = PIXI.Texture.fromImage("blob2.png");
   var firstGroup = new TeamView(stage, team, texture);
   $('body').append(firstGroup.render().el)
@@ -17,70 +17,9 @@ window.onload = function(){
 
   requestAnimationFrame( animate );
 
-  // create a texture from an image path
-  
-
-  // var structure = team.groupOptions()[0]
-  // var groups =[];
-  // var spacing = 20;
-  
-  // create a new Sprite using the texture
-  //set in line
-  // for(var i=0;i<structure.numGroups;i++){
-  //   var group = [];
-  //   group.id = i;
-  //   groups.push(group)
-  //   for(var j=0;j<structure.sizeGroup;j++){
-  //     console.log('adding group member', j)
-  //     blob =  new PIXI.Sprite(texture)
-  //     blob.anchor.x = 0.5;
-  //     blob.anchor.y = 0.5;
-  //     var moveIndex = (i*structure.sizeGroup) + j;
-  //     blob.position.x = 200 + (spacing*moveIndex);
-  //     blob.position.y = 150;
-  //     group.push(blob);
-  //     stage.addChild(blob);
-  //   }  
-  // }
-
-
-  // var steps = 0;
-  // var numSteps = 60;
-  // var groupLength = spacing * structure.sizeGroup;
   function animate() {
-
       requestAnimationFrame( animate );
-
-      // just for fun, lets rotate mr rabbit a little
-      // blob.rotation += 0.1;7
-
-      firstGroup.changeFormation(); 
-      //move the parts to change formation
-
-      // if (steps < numSteps){
-      //   groups.forEach(function(group){
-      //     group.forEach(function(blob){
-      //       blob.position.y = blob.position.y + (0.5*group.id);
-      //       var xMoveAmount = (groupLength * group.id)/numSteps; 
-      //       blob.position.x = blob.position.x - xMoveAmount;
-      //     })
-      //   });
-      //   steps++;
-      // }
-
-
-      // if (steps < 40){
-      //   for(var i=0;i<structure.numGroups;i++){
-      //     console.log('moving group', i);
-      //     for(var j=0;j<structure.sizeGroup;j++){
-      //       blob = groups[i][j]
-      //       // blob.position.x = blob.position.x + 1;
-      //       blob.position.y = blob.position.y + 2*;
-      //     }  
-      //   }
-      // }
-
-      // render the stage   
+      firstGroup.changeFormation();  
       renderer.render(stage);
   }
 }
--- app/client/team_view.js
+++ app/client/team_view.js
@@ -1,29 +1,58 @@
 var View = require('ampersand-view');
 
 var TeamView = View.extend({
-  template: '<div> </div>',
+  template: '<div></div>',
   moveSteps: 60,
   spacing: 20,
   step:0,
 
   groupLength:60,
+  events: {
+    'click .structure': 'changeStructure'
+  },
 
   initialize: function (stage, team, texture) {
     console.log('init', this.moveSteps)
     this.stage = stage;
     this.team = team;
     this.texture = texture;
-    this.groups = []
-    this.structure = this.team.groupOptions()[0]
+    this.groups = [];
+    this.structure = this.team.groupOptions()[0];
   },
   render:function(){
     this.renderWithTemplate(this);
+    this.addBlobs();
     this.basePosition();
+    this.showStructureOptions();
     return this;
+  },
+
+  changeStructure:function(ev){
+    if(ev){
+      ev.preventDefault; 
+    }
+    console.log('event', ev)
+    console.log('index', ev.delegateTarget.dataset.index)
+    index = ev.delegateTarget.dataset.index
+    this.structure = this.team.groupOptions()[index]
+    this.clear();
+    this.addBlobs();
+    this.basePosition();
+    this.step = 0;
+  },
+
+  showStructureOptions:function(){
+    this.team.groupOptions().forEach(function(structure,i){
+      console.log('structure', structure)
+      link = "<button class='structure' data-index=" + i+ ">" + structure.sizeGroup + "</button>"
+      console.log('this.el', this.el)
+      $(this.el).append(link);
+    },this)
   },
 
   changeFormation: function(){   
     if (this.step < this.moveSteps){
+      console.log('changing formation size group', this.structure.sizeGroup)
       var groupLength = this.spacing * this.structure.sizeGroup;
       this.groups.forEach(function(group){
         group.forEach(function(blob){
@@ -37,6 +66,26 @@ var TeamView = View.extend({
   },
 
   basePosition:function(){
+    this.groups.forEach(function(group,i){
+      group.forEach(function(blob,j){
+        blob.position.y = 150;
+        var moveIndex = (group.id*this.structure.sizeGroup) + j;
+        blob.position.x = 200 + (this.spacing*moveIndex);;
+      },this)
+    },this);
+
+  },
+
+  clear:function(){
+    this.groups.forEach(function(group,i){
+      group.forEach(function(blob,j){
+        this.stage.removeChild(blob);
+      },this)
+    },this);
+    this.groups = [];  
+  },
+
+  addBlobs:function(){
     this.step = 0;
     for(var i=0;i<this.structure.numGroups;i++){
       var group = [];
@@ -46,9 +95,6 @@ var TeamView = View.extend({
         blob =  new PIXI.Sprite(this.texture);
         blob.anchor.x = 0.5;
         blob.anchor.y = 0.5;
-        var moveIndex = (i*this.structure.sizeGroup) + j;
-        blob.position.x = 200 + (this.spacing*moveIndex);
-        blob.position.y = 150;
         group.push(blob);
         this.stage.addChild(blob);
       }  
